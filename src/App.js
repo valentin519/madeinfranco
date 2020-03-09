0d7db5a7c1a959882e285b4aeec659db3808678d
valentin519 <valentinjulien93@gmail.com>
2020-03-09 14:49:48 +0100

    t

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactLoading from 'react-loading';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src='/logofrancvFinal.png' alt=''/>
+      <div className='description'>
+      <p>Le monde francophone est un espace culturel, linguistique et économique unique.</p>
+<p>
+C'est avant tout un rassemblement d'êtres vivant sur différents continents qui ont une langue et des valeurs en partage.
+</p>
+<p>
+Une richesse, une diversité vibrante qu’il faut protéger, nourrir, accompagner, promouvoir avec des actions qui insufflent le sens du partage, de la solidarité, de l’inclusion indissociables de cette diversité.
+</p>
+<p>
+C’est ce qui anime « Made in Francophonie », association française loi 1901.</p>
+</div>
+      <div className='text'>
+        <p>Site en cours de construction </p>
+        <ReactLoading 
+                  className='loading' 
+                  type='bubbles'
+                  color='black'
+              />
+      </div>
+      <p>Nous serons heureux de vous acceuillir bientôt !</p>
     </div>
   );
 }
