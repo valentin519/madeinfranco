3dcb9a66852c89dcd39864222357488eebd9922f
valentin519 <valentinjulien93@gmail.com>
2020-04-14 10:56:14 +0200

    realisation created

--- src/component/Loading.js
+++ src/component/Loading.js
@@ -34,9 +34,14 @@ class Loading extends React.Component {
                 />
                
                  }
-                <p>
-                    "La Francophonie est fondatrice d'Humanité".</p>
-                <p>François H. de BEAULIEU  (  La Francophonie au 21ème siècle, Harvard Faculty Club, 2006)</p>
+                 <div className='firstLign'>             
+                    <p>
+                        "La Francophonie est fondatrice d'Humanité".
+                    </p>
+                </div>
+
+                <p>François H. de Beaulieu </p>
+                <p>"La Francophonie au 21ème siècle" , Harvard Faculty Club, 2006.</p>
                 {this.state.button &&
                 <Fade bottom>
                 <button onClick={this.props.closeModal}> Entrer</button>
--- src/component/Realisation.js
+++ src/component/Realisation.js
@@ -1,17 +1,74 @@
-import React from 'react';
+
 import './realisation.css';
 import Fade from 'react-reveal/Fade';
+import React from 'react';
+import Navbar from './Navbar';
+import YouTube from 'react-youtube';
+import Slide from 'react-reveal/Slide';
 
-const Realisation= () => (
-    <Fade left>
-    <div className='realisation' id='realisation'>
-        <p>Contenu</p>
-        <p>Contenu</p>
-        <p>Contenu</p>
-        <p>Contenu</p>
-        <p>Contenu</p>
-    </div>
-    </Fade>
-)
+
+class Realisation extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+           
+        }
+
+    }
+    _onReady(event) {
+
+    }
+
+    _onEnd(event) {
+        event.target.playVideo();
+    }
+    render() {
+        const videoOptions = {
+            playerVars: {
+                autoplay: 0,
+                controls: 1,
+                rel: 0,
+                showinfo: false
+        }
+    }
+        return(
+            <Fade left>
+            <div className='realisation' id='realisation'>
+                <div className='defileReims'>
+                    <Slide left>
+                        <YouTube
+                            className='video1'
+                            videoId="lx2ZmdWX-eA"
+                            opts={videoOptions}
+                            autoPlay="autoplay"
+                            showInfo='false'
+                            controls='true'
+                            onReady={this._onReady}
+                            onEnd={this._onEnd}
+                        />
+                        <p>Concours des Jeunes Designers Africains Francophones.</p> 
+                    </Slide>       
+                </div> 
+                <div className='defileParis'>
+                    <Slide left>
+                        <YouTube
+                            className='video2'
+                            videoId="BpoXYwJTRNg"
+                            opts={videoOptions}
+                            autoPlay="autoplay"
+                            showInfo='false'
+                            controls='true'
+                            onReady={this._onReady}
+                            onEnd={this._onEnd}
+                        />
+                        <p>Défilé du 10 mars 2020 au Palais d'Iéna, Paris.</p> 
+                    </Slide>       
+                </div>   
+            </div>
+            </Fade>
+        )
+    }
+    
+};
 
 export default Realisation;
