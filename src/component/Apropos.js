a99bbf580e1956585af4b0d351d1c0dec3a24c00
valentin519 <valentinjulien93@gmail.com>
2020-04-14 16:16:36 +0200

    realisation

--- src/component/Apropos.js
+++ src/component/Apropos.js
@@ -9,16 +9,10 @@ const Apropos = () => (
         <div className='boiteTexte'>
             <p>
                 Le monde francophone est un espace culturel, linguistique et économique unique.
-            </p>
-            <p>
                 C'est avant tout un rassemblement d'êtres vivant sur différents continents qui ont une langue et des valeurs en partage.
-            </p>
-            <p>
-                Une richesse, une diversité vibrante qu’il faut protéger, nourrir, accompagner, promouvoir avec des actions qui insufflent le sens du partage, de la solidarité, de l’inclusion indissociables de cette diversité.
-            </p>
-            <p>
-                C’est ce qui anime « Made in Francophonie », association française loi 1901.
-            </p>
+                Une richesse, une diversité vibrante qu’il faut protéger, nourrir, accompagner, promouvoir avec des actions qui insufflent le sens du partage, de la solidarité, de l’inclusion indissociables de cette diversité. Des actions qui s'inscrivent également dans les Objectifs de Développement Durable de l'ONU pour 2030.
+                C’est ce qui anime l'équipe de « Made in Francophonie », association française loi 1901, dans ses projets, ses réalisations que nous vous proposons de découvrir ici...
+            </p>       
         </div>
     </div>
     </Fade>
--- src/component/Realisation.js
+++ src/component/Realisation.js
@@ -37,7 +37,7 @@ class Realisation extends React.Component {
                 <div className='defileReims'>
                     <Slide left>
                         <YouTube
-                            className='video1'
+                            className='video'
                             videoId="lx2ZmdWX-eA"
                             opts={videoOptions}
                             autoPlay="autoplay"
@@ -46,9 +46,27 @@ class Realisation extends React.Component {
                             onReady={this._onReady}
                             onEnd={this._onEnd}
                         />
-                        <p>Concours des Jeunes Designers Africains Francophones.</p> 
+                        <div className='text1'>
+                        <p>
+                            Mode d'Avenir 
+                            Avec le designer de mode Ousmane Ouedraogo ("Ousman O."), Made-in-Francophonie a créé le Concours des Jeunes Designers de Mode Francophones dont la 1ère édition, en 2019, était dédiée aux jeunes créateurs africains avec le parrainage de Pathé'O, le couturier de Nelson Mandela, Desmond Tutu, Kofi Annan ou encore Mary Robinson, Noémie Campbell... Les lauréats ont été invités à présenter leurs modèles à l'occasion du défilé de clôture à Reims, le 26 septembre 2019, au prestigieux Palais du Tau.
+                            Cette initiative vise à promouvoir l'entreprenariat, la création dans les pays africains francophones en offrant  aux lauréats visibilité, accès aux médias et aux marchés. L'initiative vise également à soutenir le travail des coopératives de femmes dans ces différents pays, qui tissent et teignent les tissus traditionnels, et à perpétuer ainsi un savoir-faire ancestral. Cette démarche contribue à l'autonomisation économique de ces femmes et des jeunes, également à une égalité des chances entre homme et femme parmi les lauréats. Il s'agit aussi de promouvoir des alternatives (coton bio, teintures végétales, chanvre...) qui peuvent faire une réelle différence dans une industrie qui se classe au second rang parmi les plus polluantes de la planète.
+                            Le label "Made in Francophonie" qui accompagne toutes ces créations différentes, indique, en plus du nom du designer, celui des artisans qui ont contribué à la réalisation du modèle: pour une mode éthique et solidaire...
+                        </p>
+                        </div> 
                     </Slide>       
                 </div> 
+                <div className='unionJournal'>
+                    <a href="#union" className='picUnion'>
+                        <img src="/union.jpg" alt="Vignette" className='union'/>
+                    </a>
+                    <a href="#_" class="overlay" id="union">
+                        <img src="/union.jpg" alt="Plein écran"/>
+                    </a>
+                    <div className='text3'>
+                        <p>Journal régional L'Union découvre, à cette occasion, Made In Francophonie.</p>
+                    </div>
+                </div>
                 <div className='defileParis'>
                     <Slide left>
                         <YouTube
@@ -61,9 +79,12 @@ class Realisation extends React.Component {
                             onReady={this._onReady}
                             onEnd={this._onEnd}
                         />
-                        <p>Défilé du 10 mars 2020 au Palais d'Iéna, Paris.</p> 
+                        <div className='text2'>
+                            <p>Le CESE invitait Made in Francophonie le 10 mars 2019, dans le cadre des 50 ans de la Francophonie, au Palais d'Iéna à Paris. L'occasion de présenter un défilé d'Ousman O. dont chaque modèle portait le label "Made in Francophonie".</p> 
+                        </div>
                     </Slide>       
-                </div>   
+                </div> 
+                
             </div>
             </Fade>
         )
