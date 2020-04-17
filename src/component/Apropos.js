8b064985419b7b6b9a03a7b4d79e752342b37af1
valentin519 <valentinjulien93@gmail.com>
2020-04-17 13:36:33 +0200

    text structured

--- src/component/Apropos.js
+++ src/component/Apropos.js
@@ -8,9 +8,10 @@ const Apropos = () => (
         <img src='logofran.png' alt='' />
         <div className='boiteTexte'>
             <p>
-                Le monde francophone est un espace culturel, linguistique et économique unique.
-                C'est avant tout un rassemblement d'êtres vivant sur différents continents qui ont une langue et des valeurs en partage.
+                Le monde francophone est un espace culturel, linguistique et économique unique.<br/>
+                C'est avant tout un rassemblement d'êtres vivant sur différents continents qui ont une langue et des valeurs en partage.<br/>
                 Une richesse, une diversité vibrante qu’il faut protéger, nourrir, accompagner, promouvoir avec des actions qui insufflent le sens du partage, de la solidarité, de l’inclusion indissociables de cette diversité. Des actions qui s'inscrivent également dans les Objectifs de Développement Durable de l'ONU pour 2030.
+                <br/>
                 C’est ce qui anime l'équipe de « Made in Francophonie », association française loi 1901, dans ses projets, ses réalisations que nous vous proposons de découvrir ici...
             </p>       
         </div>
